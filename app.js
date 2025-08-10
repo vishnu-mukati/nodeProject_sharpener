1d5df31e654a1bc5480a3ec5b006c16976eaaf64
vishnu-mukati <mukativishnu1232@gmaila.com>
2025-08-10 19:47:09 +0530

    read and write the file by the stream and the events

--- app.js
+++ app.js
@@ -1,30 +1,56 @@
 const http = require('http');
-
-const server = http.createServer((req,res)=>{
+const fs = require('fs');
+const server = http.createServer((req, res) => {
     console.log("server is created");
 
-    res.setHeader("content-type","text/html");
+    const url = req.url;
+    const method = req.method;
 
-    if(req.url==="/"){
-        req.statusCode= 200;
-        res.end("<h1>Hello World</h1>");
+    if (url === "/") {
+        res.setHeader("content-type", "text/html");
+        res.end(
+            `
+            <form action="/message" method="POST">
+            <label>name:</label>
+            <input type="text" name="username"/>
+            <button type="submit">Add</button>
+            </form>
+            `
+        )
     }else{
-        res.statusCode = 200;
-        if(req.url ==="/home"){
-            res.end("<h1>Welcome Home</h1>");
-        }else if(req.url ==="/about"){
-             res.end("<h1>Welcome to About Us</h1>")
-        }else if(req.url ==="/node"){
-            res.end("<h1>Welcome to my Node Js project</h1>")
-        }
-        else{
-            res.statusCode = 404;
-            res.end("<h1>page not found</h1>");
+        if(url === "/message"){
+            res.setHeader("content-type","text/html");
+            const dataChunks = [];
+            req.on("data",(chunks)=>{
+                console.log(chunks);
+              dataChunks.push(chunks);
+            })
+
+            req.on("end",()=>{
+                let combinedData = Buffer.concat(dataChunks);
+                console.log(combinedData.toString());
+                const value = combinedData.toString().split("=")[1];
+                console.log(value);
+
+                fs.writeFile("formValues.txt",value,(err)=>{
+                     res.statusCode=302;//redirection
+                     res.setHeader('location','/')
+                     res.end();
+                })
+            })
+        }else{
+            if(req.url ==="/read"){
+                fs.readFile('formValues.txt',(err,data)=>{
+                    console.log(data.toString());
+                    res.end(`
+                        <h1>${data.toString()}</h1>`);
+                })
+            }
         }
     }
 })
 
 const port = 3000;
-server.listen(port,()=>{
+server.listen(port, () => {
     console.log("server is running");
 });
